05071849e92c10cc72b2e299e5481a6bf4af98e6
Adam Lynch <alynch14@students.towson.edu>
2019-10-29 21:35:09 -0400

    Started implementation of SJF algorithm

--- src/main/java/schedulers/ShortestJobFirstScheduler.java
+++ src/main/java/schedulers/ShortestJobFirstScheduler.java
@@ -2,21 +2,37 @@ package schedulers;
 
 import process.Process;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ShortestJobFirstScheduler implements CpuSchedulingAlgorithm {
     private List<Process> processList = new ArrayList<Process>();
+    private String ganttChart;
 
     public void setProcesses(List<Process> processList) {
         this.processList.addAll(processList);
     }
 
     public void runAlgorithm() {
+        StringBuilder mainChart = new StringBuilder("|");
+        String times = "0";
+        int lastProcessBurstTime = 0;
 
+        sortData();
+        for(Process process : processList){
+            mainChart.append(process.getProcessId()).append("|");
+//            times += " " +
+        }
+    }
+
+    private void sortData(){
+        for(Process process : processList){
+            process.setProcessBurstTime((process.getProcessArrivalTime() + process.getProcessBurstTime()));
+        }
+
+        processList.sort(Comparator.comparing(Process::getProcessBurstTime));
     }
 
     public String getGanttChart() {
-        return null;
+        return ganttChart;
     }
 }
